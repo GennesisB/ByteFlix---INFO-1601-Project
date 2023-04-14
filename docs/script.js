2d25cf061a3014ab96232d8adeb994c2cdb366f0
Gennesis Bethelmy <126026755+GennesisB@users.noreply.github.com>
2023-04-14 17:31:22 +0000

    Initial commit

---
+++ docs/script.js
@@ -0,0 +1,104 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?api_key=760c22fcfd8f24e3d9f1377d9c5749ec";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=760c22fcfd8f24e3d9f1377d9c5749ec&query=";
+
+const UPCOMINGAPI=  "https://api.themoviedb.org/3/movie/upcoming?api_key=760c22fcfd8f24e3d9f1377d9c5749ec&language=en-US&page=1";
+
+const NOW_PLAYINGAPI = "https://api.themoviedb.org/3/movie/now_playing?api_key=760c22fcfd8f24e3d9f1377d9c5749ec&language=en-US&page=1";
+
+
+const main = document.getElementById("content");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+
+
+getMovies(NOW_PLAYINGAPI);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    showMovies(respData.results);
+}
+
+function showMovies(movies) {
+    
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
+
+
+  
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+
+
+
---
+++ docs/script2.js
@@ -0,0 +1,82 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?api_key=760c22fcfd8f24e3d9f1377d9c5749ec";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=760c22fcfd8f24e3d9f1377d9c5749ec&query=";
+
+const UPCOMINGAPI=  "https://api.themoviedb.org/3/movie/upcoming?api_key=760c22fcfd8f24e3d9f1377d9c5749ec&language=en-US&page=1";
+
+const NOW_PLAYINGAPI = "https://api.themoviedb.org/3/movie/now_playing?api_key=760c22fcfd8f24e3d9f1377d9c5749ec&language=en-US&page=1";
+
+
+const main = document.getElementById("content");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main_recently = document.getElementById("content");
+const form_recently = document.getElementById("form_2");
+const search_recently = document.getElementById("search_2");
+
+
+
+
+getMovies(UPCOMINGAPI);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    showMovies(respData.results);
+}
+
+function showMovies(movies) {
+    
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
